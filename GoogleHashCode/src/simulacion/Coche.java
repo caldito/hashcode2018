e9a13c6c1a86a56d9ccabe69b85a2a3842dfe537
Unknown <100383255@alumnos.uc3m.es>
2018-05-16 18:47:47 +0200

    Corre todos los casos en la misma ejecucion

--- GoogleHashCode/src/simulacion/Coche.java
+++ GoogleHashCode/src/simulacion/Coche.java
@@ -2,23 +2,14 @@ package simulacion;
 import java.util.*;
 
 public class Coche {
-	int numero;
-	int x = 0;
-	int y = 0;
+	int x;// = 0;
+	int y;// = 0;
 	int arriving_time = -1;
 	ArrayList rides = new ArrayList();
 
-	
-	Coche (int numero){
-		this.numero = numero;
-		//int x = 0;
-		//int y = 0;
-		//int arriving_time = -1;
-		//ArrayList rides = new ArrayList();
-		
-		
-		
-		
+
+	public Coche (){
+
 	}
 	
 	
--- GoogleHashCode/src/simulacion/Main.java
+++ GoogleHashCode/src/simulacion/Main.java
@@ -5,211 +5,189 @@ import java.io.*;
 //import java.io.FileReader;
 import list.List;
 import list.SNode;
-import java.util.Scanner;
+//import java.util.Scanner;
 
 
 public class Main {
 
-	public static List generateList(String ruta) {
+	private static List generateList(String ruta) {
 		List lista = new List();
-		File archivo = null;
+		File archivo;
 		FileReader fr = null;
-		BufferedReader br = null;
-	
+		BufferedReader br;
+
 		try {
-			archivo = new File (ruta);
-			fr = new FileReader (archivo);
+			archivo = new File(ruta);
+			fr = new FileReader(archivo);
 			br = new BufferedReader(fr);
 			String linea;
-			while((linea=br.readLine())!=null)
-			   addRide(linea,lista);
-		}
-		catch(Exception e){
+			while ((linea = br.readLine()) != null)
+				addRide(linea, lista);
+		} catch (Exception e) {
 			e.printStackTrace();
-		}finally{
-			try{                    
-				if( null != fr ){   
-					fr.close();     
-				}                  
-			}catch (Exception e2){ 
+		} finally {
+			try {
+				if (null != fr) {
+					fr.close();
+				}
+			} catch (Exception e2) {
 				e2.printStackTrace();
 			}
 		}
 		return lista;
 	}
-	
-	public static void addRide(String linea,List lista) {	
-		int [] ride = new int [6];
+
+	 private static void addRide(String linea, List lista) {
+		int[] ride = new int[6];
 		String sNum = "";
-		for (int i = 0,k = 0;i<linea.length() && k<6;i++) {
-			if (linea.charAt(i) != ' ' && i != linea.length()-1) {
+		for (int i = 0, k = 0; i < linea.length() && k < 6; i++) {
+			if (linea.charAt(i) != ' ' && i != linea.length() - 1) {
 				sNum += linea.charAt(i);
-			}else if(linea.charAt(i) != ' ' && i == linea.length()-1) {
+			} else if (linea.charAt(i) != ' ' && i == linea.length() - 1) {
 				sNum += linea.charAt(i);
 				ride[k] = Integer.parseInt(sNum);
 				sNum = "";
 				k++;
-			}else {
+			} else {
 				ride[k] = Integer.parseInt(sNum);
 				sNum = "";
 				k++;
-				
+
 			}
 		}
 		lista.addLast(ride);
 	}
-	
-	public static void generate_output(String ruta, Coche[]coches) {
-		FileWriter fichero = null;
-        PrintWriter pw = null;
-        try
-        {
-            fichero = new FileWriter(ruta);
-            pw = new PrintWriter(fichero);
 
-            for (int j = 0;j<coches.length;j++) {
+	private static void generate_output(String ruta, Coche[] coches) {
+		FileWriter fichero = null;
+		PrintWriter pw;
+		try {
+			fichero = new FileWriter(ruta);
+			pw = new PrintWriter(fichero);
+			for (Coche coche : coches) {//int j = 0; j < coches.length; j++
 				String linea = "";
-            	linea += coches[j].rides.size();
-				for (int k = 0;k<coches[j].rides.size();k++) {
-					linea += " "+coches[j].rides.get(k);
+				linea += coche.rides.size();
+				for (int i = 0; i < coche.rides.size(); i++) {
+					linea += " " + coche.rides.get(i);
 				}
 				pw.println(linea);
-				
-			}
-            
 
-        } catch (Exception e) {
-            e.printStackTrace();
-        } finally {
-           try {
-           // Nuevamente aprovechamos el finally para 
-           // asegurarnos que se cierra el fichero.
-           if (null != fichero)
-              fichero.close();
-           } catch (Exception e2) {
-              e2.printStackTrace();
-           }
-        }
+			}
+
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				// Nuevamente aprovechamos el finally para
+				// asegurarnos que se cierra el fichero.
+				if (null != fichero)
+					fichero.close();
+			} catch (Exception e2) {
+				e2.printStackTrace();
+			}
+		}
 	}
-	
+
 	@SuppressWarnings("unchecked")
-	public static int assignRide(Coche coche,int t, List lista, int puntos, int bonus) {
-		
-		if (coche.arriving_time == -1 || coche.arriving_time  == t) {
-			
-			int i = 0;
-			int menor = 999999999;
-			int indice_menor = -1;
-			for (SNode nodeIt = lista.first; nodeIt != null ; nodeIt = nodeIt.next,i++) {
-				int llegada = Math.abs(nodeIt.ride[0]-coche.x) + Math.abs(nodeIt.ride[1]-coche.y);
-				int espera = 0;
+	private static int assignRide(Coche coche, int t, List lista, int puntos, int bonus) {
+
+		if (coche.arriving_time == -1 || coche.arriving_time == t) {//si no tiene viaje asignado se le asigna
+
+			int i = 0, menor = 999999999,indice_menor = -1;
+			for (SNode nodeIt = lista.first; nodeIt != null; nodeIt = nodeIt.next, i++) {
+				int llegada = Math.abs(nodeIt.ride[0] - coche.x) + Math.abs(nodeIt.ride[1] - coche.y);
+				int espera;// = 0;
 				if (nodeIt.ride[4] - (t + llegada) > 0) {
 					espera = nodeIt.ride[4] - (t + llegada);
-				}else {
+				} else {
 					espera = 0;
 				}
-				int viaje = Math.abs(nodeIt.ride[2]-nodeIt.ride[0]) + Math.abs(nodeIt.ride[3]-nodeIt.ride[1] + 1);
-				
+				int viaje = Math.abs(nodeIt.ride[2] - nodeIt.ride[0]) + Math.abs(nodeIt.ride[3] - nodeIt.ride[1] + 1);
+
 				if (t + llegada + espera + viaje < menor) {
 					menor = t + llegada + espera + viaje;
 					indice_menor = i;
 				}
 			}
-			if(lista.isEmpty())
+			if (lista.isEmpty())
 				return puntos;
-			
-			
-			if(menor < lista.getAt(indice_menor).ride[5]) {
-				puntos += Math.abs(lista.getAt(indice_menor).ride[2]-lista.getAt(indice_menor).ride[0]) + Math.abs(lista.getAt(indice_menor).ride[3]-lista.getAt(indice_menor).ride[1] + 1);
-				
-				if(lista.getAt(indice_menor).ride[4] - (t + Math.abs(lista.getAt(indice_menor).ride[0]-coche.x) + Math.abs(lista.getAt(indice_menor).ride[1]-coche.y)) >= 0) {
+
+
+			if (menor < lista.getAt(indice_menor).ride[5]) {
+				puntos += Math.abs(lista.getAt(indice_menor).ride[2] - lista.getAt(indice_menor).ride[0]) + Math.abs(lista.getAt(indice_menor).ride[3] - lista.getAt(indice_menor).ride[1] + 1);
+
+				if (lista.getAt(indice_menor).ride[4] - (t + Math.abs(lista.getAt(indice_menor).ride[0] - coche.x) + Math.abs(lista.getAt(indice_menor).ride[1] - coche.y)) >= 0) {
 					puntos += bonus;
 				}
 			}
-			
-			
+
+
 			coche.rides.add(lista.getAt(indice_menor).ride_index);
 			coche.x = lista.getAt(indice_menor).ride[2];
 			coche.y = lista.getAt(indice_menor).ride[3];
 			coche.arriving_time = menor;
 			lista.removeAt(indice_menor);
-			
+
 			return puntos;
-			
+
 		}
 		return puntos;
 	}
-	
-	
+
+
 	public static void main(String[] args) {
-		
-		//Selector de archivo
-		Scanner sc = new Scanner(System.in);
-		System.out.println("¿Que simulación?\na. example\nb. should be easy\nc. no hurry\nd. metropolis\ne. high bonus");
+		int totalPuntos = 0;
 		String ruta = "";
-		String archivo = sc.next();
-		
-		switch (archivo) {
-		case "a":
-			ruta ="entradas/a_example.in";
-			break;
-		case "b":
-			ruta ="entradas/b_should_be_easy.in";
-			break;
-		case "c":
-			ruta ="entradas/c_no_hurry.in";
-			break;
-		case "d":
-			ruta ="entradas/d_metropolis.in";
-			break;
-		case "e":
-			ruta ="entradas/e_high_bonus.in";
-			break;
-		}
-		sc.close();
-		
-		
-		//Creación lista de viajes
-		List lista = generateList(ruta);
-		
-		//Generacion mapa
-		int[]city = lista.first.ride;
-		lista.removeFirst();
-		
-		//Creacion flota de coches
-		Coche [] coches = new Coche [city[2]];
-		for (int i = 0; i < city[2]; i++) {
-			coches[i] = new Coche(i + 1);
-		}
-		
-		//Bucle principal que recorre el tiempo
-		int t=0;
-		int puntos = 0;
-		while(!lista.isEmpty()) {
-			//System.out.println("Step: "+ t);
-			//System.out.println("Tamaño lista: "+lista.getSize());
-			
-			for (int i = 0; i < coches.length; i++) {//Asignar viaje si un coche no tiene viaje asignado
-				puntos = assignRide(coches[i],t, lista, puntos,city[4]);
+		for (int archivo = 0; archivo < 5; archivo++) {
+			switch (archivo) {
+				case 0:
+					ruta = "entradas/a_example.in";
+					break;
+				case 1:
+					ruta = "entradas/b_should_be_easy.in";
+					break;
+				case 2:
+					ruta = "entradas/c_no_hurry.in";
+					break;
+				case 3:
+					ruta = "entradas/d_metropolis.in";
+					break;
+				case 4:
+					ruta = "entradas/e_high_bonus.in";
+					break;
 			}
-			
-			if (lista.isEmpty()) {
-				System.out.println("Puntos: "+puntos);
-				generate_output("salidas/"+archivo+".txt", coches);
-				for (int j = 0;j<coches.length;j++) {
-					System.out.print(coches[j].rides.size());
-					for (int k = 0;k<coches[j].rides.size();k++) {
-						System.out.print(" "+coches[j].rides.get(k));
-					}
-					System.out.println();	
+			System.out.println();
+			System.out.println(ruta);
+
+			//Creación lista de viajes
+			List lista = generateList(ruta);
+
+			//Generacion mapa
+			int[] city = lista.first.ride;
+			lista.removeFirst();
+
+			//Creacion flota de coches
+			Coche[] coches = new Coche[city[2]];
+			for (int i = 0; i < city[2]; i++)//int i = 0; i < city[2]; i++
+				coches[i] = new Coche();
+
+			//Bucle principal que recorre el tiempo
+			int t = 0;
+			int puntos = 0;
+			while (!lista.isEmpty()) {
+				for (Coche coche : coches) {//Asignar viaje si un coche no tiene viaje asignado
+					puntos = assignRide(coche, t, lista, puntos, city[4]);
 				}
-				break;	
+				if (lista.isEmpty()) {
+					System.out.println("Puntos: " + puntos);
+					totalPuntos += puntos;
+					generate_output("salidas/" + ruta.charAt(9) + ".txt", coches);
+				}
+				t++;
 			}
-			
-			t++;
-		
 		}
-		
+		System.out.println("PUNTUACION TOTAL: "+totalPuntos);
 	}
-	
+
 }
